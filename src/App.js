03e9e6b022c2367b5ff2c5c561d75ba91d068779
Elena Hoshizaki <elena.hoshizaki@gmail.com>
2022-11-16 14:29:15 -0800

    add AR.js demo code

--- src/App.js
+++ src/App.js
@@ -10,6 +10,19 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
+
+      <a-scene
+        vr-mode-ui="enabled: false"
+        arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
+      >
+        <a-text
+          value="This content will always face you."
+          look-at="[gps-camera]"
+          scale="120 120 120"
+          gps-entity-place="latitude: 37.87598° N; longitude: 122.25882° W;"
+        ></a-text>
+        <a-camera gps-camera rotation-reader> </a-camera>
+      </a-scene>
     </div>
   );
 }
